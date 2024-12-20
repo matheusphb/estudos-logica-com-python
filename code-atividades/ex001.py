e4d1f06425ac7d737a2fafb6cfbd94d4385d1ea6
matheusphb <139464891+matheusphb@users.noreply.github.com>
2024-12-20 20:36:36 +0000

    alteraçãoa para melhorar

--- code-atividades/ex001.py
+++ code-atividades/ex001.py
@@ -1,13 +1,13 @@
-#Declaração de variáveis
-#Atribuição de valores
-#Nomes de variáveis(nome, tipo, xp)
-#Reatribuição de valores(xp = xp + 1500)
-#Tipos de dados(Str, Str, Int)
+# Declaração e atribuição de variáveis
+# Nomes de variáveis: nome, tipo, xp
+# Tipos de dados: str, str, int
 
-nome = "Gandalf"
-tipo = "Mago"
-xp = 1500
+nome = "Gandalf"  # Nome do personagem
+tipo = "Mago"     # Tipo do personagem
+xp = 1500         # Experiência inicial do personagem
 
-xp = xp + 1500
+# Reatribuição de valor à variável xp
+xp += 1500  # Adiciona 1500 à experiência atual
 
-print("Seu personagem é,", nome,"do tipo, ", tipo,"e xp:", xp)
+# Exibição das informações do personagem
+print(f"Seu personagem é {nome}, do tipo {tipo}, e xp: {xp}")
